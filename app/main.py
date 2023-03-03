1dfabd19b1423e0d3eae65cb3fda1345ca8232cd
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-03-03 15:49:11 +0100

    feat(main): output data

--- app/main.py
+++ app/main.py
@@ -1,24 +1,29 @@
 from typing import Union
-from app.get_cities import get_cities
+# from app.get_cities import get_cities
+from app.get_data_from_csv import root
 from fastapi import FastAPI, HTTPException
+import json
+import os
 
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root(county: str, rate: int, living_space: int):
-    cities = get_cities(county, rate, living_space)
-    if cities is None:
-        raise HTTPException(status_code=500, detail="Items not found")
+async def read_root(county_code: str, rate: int, living_space: int):
     output = []
-    for city in cities:
-        row = {
-            'code postale': city.code_postal,
-            'ville': city.ville,
-            'population': int(city.habitants.replace(' ', '')),
-            'note': float(city.note_globale) if city.note_globale != '-' else city.note_globale,
-            'loyer moyen': city.loyer_moyen
-        }
-        output.append(row)
-    return output
+    with open(os.path.join(root, 'data', 'data.json')) as f:
+        data_json = json.loads(f.read())
+    for i, row in enumerate(data_json):
+        if list(row.keys()).pop() == county_code:
+            for city in list(row.values()).pop()['cities']:
+                output.append({
+                    'code postale': city['code_postal'],
+                    'ville': city['ville'],
+                    'population': int(city['habitants'].replace(' ', '')),
+                    'note': float(city['note_globale']) if city['note_globale'] != '-' else city['note_globale'],
+                    'loyer moyen': int(city['loyer']) * (living_space / rate)
+                })
+            return output
+        if i == len(data_json) - 1:
+            return HTTPException(status_code=404, detail="Item not found")
