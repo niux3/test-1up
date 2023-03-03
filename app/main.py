d5ac9f8ec920e7643065bd0a32f8adc9d4ad05fc
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-03-03 19:07:29 +0100

    feat(project): clean code

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from app.get_data_from_csv import root
-from fastapi import FastAPI, HTTPException
+"""
+fichier d'execution fastapi
+"""
 import json
 import os
+from fastapi import FastAPI, HTTPException
+from app.get_data_from_csv import root
 
 
 app = FastAPI()
@@ -9,17 +12,20 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root(county_code: str, rate: int, living_space: int):
+    """index project"""
     output = []
-    with open(os.path.join(root, 'data', 'data.json')) as f:
-        data_json = json.loads(f.read())
+    path = os.path.join(root, 'data', 'data.json')
+    with open(path, encoding="utf-8") as file:
+        data_json = json.loads(file.read())
     for i, row in enumerate(data_json):
         if list(row.keys()).pop() == county_code:
             for city in list(row.values()).pop()['cities']:
+                note = city['note_globale']
                 output.append({
                     'code postale': city['code_postal'],
                     'ville': city['ville'],
                     'population': int(city['habitants'].replace(' ', '')),
-                    'note': float(city['note_globale']) if city['note_globale'] != '-' else city['note_globale'],
+                    'note': float(note) if note != '-' else note,
                     'loyer moyen': int(city['loyer']) * (living_space / rate)
                 })
             return output
--- app/save_data_json.py
+++ app/save_data_json.py
@@ -1,30 +1,39 @@
+"""
+save json file
+"""
+import json
+import os
+from collections import namedtuple
 from get_cities import get_zipcode
 from get_data_from_csv import root, get_rent
 from requests import get
 from bs4 import BeautifulSoup
 from slugify import slugify
-from collections import namedtuple
-import json
-import os
 
 
 def get_county_list_from_http():
+    """get cities data"""
     data_to_json = []
-    r = get('https://www.bien-dans-ma-ville.fr/departements/')
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text, 'html.parser')
+    req = get('https://www.bien-dans-ma-ville.fr/departements/')
+    if req.status_code == 200:
+        soup = BeautifulSoup(req.text, 'html.parser')
         tds = soup.select('#liste table td')
         for td in tds:
             if td.find('a') is not None:
                 href = td.find('a')['href']
-                r = get(href)
-                if r.status_code == 200:
-                    soup = BeautifulSoup(r.text, 'html.parser')
+                req = get(href)
+                if req.status_code == 200:
+                    soup = BeautifulSoup(req.text, 'html.parser')
                     county_name = td.find('a').string
                     county_code = soup.select('h1 small').pop().string
                     header = soup.select('#villesvoisines table th')
-                    fields = f"code_postal loyer {' '.join([slugify(cell.string, separator='_') for cell in header])}"
-                    City = namedtuple('City', fields)
+                    default_fields = [
+                        slugify(cell.string, separator='_')
+                        for cell in header
+                    ]
+                    str_fields = ' '.join(default_fields)
+                    fields = f"code_postal loyer {str_fields}"
+                    city_tuple = namedtuple('City', fields)
                     len_fields = len(header)
                     data = soup.select('#villesvoisines table tr td')
                     output_cities = []
@@ -34,13 +43,21 @@ def get_county_list_from_http():
                             row = []
                             city = cell.string
                             zip_code = get_zipcode(cell.find('a')['href'])
-                            print('>>> ', county_code, ' >> ', city, ' >> ', zip_code)
+                            params_print = [
+                                '> ',
+                                county_code,
+                                ' > ',
+                                city,
+                                ' > ',
+                                zip_code
+                            ]
+                            print(*params_print)
                             row.append(zip_code)
                             row.append(get_rent(city))
                             row.append(city)
                         elif i % len_fields == len_fields - 1:
                             row.append(cell.string)
-                            output_cities.append(City(*row)._asdict())
+                            output_cities.append(city_tuple(*row)._asdict())
                         else:
                             row.append(cell.string)
                 data_to_json.append({
@@ -56,9 +73,10 @@ def get_county_list_from_http():
 
 
 def save_data(rows):
-    print(root)
-    with open(os.path.join(root, 'data', 'data.json'), 'w') as f:
-        f.write(json.dumps(rows, indent=4))
+    """save data in folder"""
+    path = os.path.join(root, 'data', 'data.json')
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(json.dumps(rows, indent=4))
 
 
 if __name__ == '__main__':
